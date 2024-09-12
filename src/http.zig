f7662ba992ecc52dc5b1fbd9dd02fdcec4bc1206
Abdhilahi R. Wabwire <124460109+AbdhilahiRWabwire@users.noreply.github.com>
2024-09-12 07:57:31 -0600

    FInish References

---
+++ src/http.zig
@@ -0,0 +1,49 @@
+const standard: type = @import("std");
+
+// HTTP Type: Structure
+const HTTP = struct {
+    // HTTP CONNECT Method
+    fn connect() u8 {
+        return 0;
+    }
+
+    // HTTP DELETE Method
+    fn delete() u8 {
+        return 0;
+    }
+
+    // HTTP GET Method
+    fn get() u8 {
+        return 0;
+    }
+
+    // HTTP HEAD Method
+    fn head() u8 {
+        return 0;
+    }
+
+    // HTTP OPTIONS Method
+    fn options() u8 {
+        return 0;
+    }
+
+    // HTTP PATCH Method
+    fn patch() u8 {
+        return 0;
+    }
+
+    // HTTP POST Method
+    fn post() u8 {
+        return 0;
+    }
+
+    // HTTP PUT Method
+    fn put() u8 {
+        return 0;
+    }
+
+    // HTTP TRACE Method
+    fn trace() u8 {
+        return 0;
+    }
+};
---
+++ src/permssion_policy.zig
@@ -0,0 +1,11 @@
+// HTTP Permission Policy Directives 
+const permissionPolicyDirectives: [8]u8 = [8]u8 {
+    "camera",
+    "display-capture",
+    "fullscreen",
+    "geolocation",
+    "microphone",
+    "publickey-credentials-get",
+    "screen-wake-lock",
+    "web-share",
+};
