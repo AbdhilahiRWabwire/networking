df819c4e10e942a263ef533232e7ad1befd46524
Abdhilahi R. Wabwire <124460109+AbdhilahiRWabwire@users.noreply.github.com>
2024-09-15 03:02:07 -0600

    Fill Parameters

--- src/http/http_message.zig
+++ src/http/http_message.zig
@@ -5,12 +5,13 @@ const network: type = standard.net;
 // Hypertext Transfer Protocol Message Model
 const HTTPMessage: type = struct {
     // Hypertext Transfer Protocol Response Message
-    fn response() u8 {
+    fn response(body: ?u8, headers: u8, statusCode: u8, statusMessage: u8, version: u8) u8 {
         return 0;
     }
     
     // Hypertext Transfer Protocol Request Message
-    fn request() u8 {
+    fn request(body: ?u8, headers: u8, method: u8, path: u8, version: u8) u8 {
+
         return 0;
     }
 };
