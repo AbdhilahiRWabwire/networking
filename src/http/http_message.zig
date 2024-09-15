e98b1eca8060f85f7336159833f693a559a7afb2
Abdhilahi R. Wabwire <124460109+AbdhilahiRWabwire@users.noreply.github.com>
2024-09-15 02:45:33 -0600

    FIx Typo

--- src/http/http_message.zig
+++ src/http/http_message.zig
@@ -1,5 +1,7 @@
 const standard: type = @import("std");
 
+const network: type = standard.net;
+
 // Hypertext Transfer Protocol Message Model
 const HTTPMessage: type = struct {
     // Hypertext Transfer Protocol Response Message
